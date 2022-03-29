9681f9f811dc1dd63494f6fab0cd277f5ced81c2
Michael Carr <xyeres@me.com>
2022-03-29 11:53:28 -0700

    :bulb: Add example page

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+
 module.exports = {
   mode: 'development', // 1
   entry: {
-    carousel: './src/Carousel.js', // 2
+    carousel: './src/Carousel.js',
+    example: './example/index.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Carousel Example', //1
+      chunks: ['example'], //2
+    }),
+  ],
   module: {
     // 3
     rules: [
